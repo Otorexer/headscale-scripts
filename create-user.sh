9a62a5ba3e7c3d479519439efd75e0bd8b615c59
Otorexer <118485801+Otorexer@users.noreply.github.com>
2023-06-15 11:53:00 +0200

    Update create-user.sh

--- create-user.sh
+++ create-user.sh
@@ -1,17 +1,20 @@
 #!/bin/s
-
-#Variables of colors
+clear
+#Colors
 clear='\033[0m'
+red='\033[0;31m'
 green='\033[0;32m'
-
+yellow='\033[0;33m'
+blue='\033[0;34m'
+magenta='\033[0;35m'
+cyan='\033[0;36m'
 
 #Script for creating user
 ip=$(sed '13q;d' /etc/headscale/config.yaml)
 ip=$(cut -c 13-200 <<< $ip)
-echo "Name of the User?"
+echo -e "${cyan}Name of the User?${clear}"
 read name
 headscale users create $name
 key=$(headscale --user $name preauthkeys create --reusable --expiration 30m)
-printf "${green}Use this command to login to Tailscale${clear}"
-echo
+echo -e "${cyan}Use this command to login to Tailscale${clear}"
 echo "tailscale up --login-server $ip --authkey $key"
